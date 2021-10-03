48e0eb43d317f4978f916db794529bfc12887078
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-10-03 17:46:52 -0300

    Literal types

--- section-2-typescript-basics-and-basics-types/app.ts
+++ section-2-typescript-basics-and-basics-types/app.ts
@@ -1,13 +1,30 @@
 // Union types
-function combine(n1: number | string, n2: number | string) {
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        return n1 + n2
+// function combine(n1: number | string, n2: number | string) {
+//     if (typeof n1 === 'number' && typeof n2 === 'number') {
+//         return n1 + n2
+//     }
+//     return n1.toString() + n2.toString()
+// }
+
+// const combineAges = combine(30, 26);
+// console.log(combineAges);
+
+// const combineAges1 = combine('30', '26');
+// console.log(combineAges1);
+
+// Literal types
+function combine(n1: number | string, n2: number | string, resultConversion: 'as-number' | 'as-text') {
+    if (typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number') {
+        return +n1 + +n2
     }
     return n1.toString() + n2.toString()
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number');
 console.log(combineAges);
 
-const combineAges1 = combine('30', '26');
+const combineAges1 = combine('30', '26', 'as-text');
 console.log(combineAges1);
+
+const combineAges2 = combine('30', '26', 'as-number');
+console.log(combineAges2);
