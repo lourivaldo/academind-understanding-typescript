5ae1e2dadcd16be285000fad89869e8f96b39f32
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2021-10-10 17:05:52 -0300

    Function Overloads

--- section-6-advanced-types/src/app.ts
+++ section-6-advanced-types/src/app.ts
@@ -153,3 +153,19 @@ const errorBag: ErrorContainer = {
     email: 'Not valid',
     username: 'Must start with a capital character',
 }
+
+
+// Function Overloads
+
+function add2(a: number, b: number): number
+function add2(a: string, b: string): string
+function add2(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+const result = add2(1, 3)
+const result2 = add2('A', 'B')
+
